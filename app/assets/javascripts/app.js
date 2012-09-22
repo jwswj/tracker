21ab2125bfb9b872ef0f55ccccf5910c30f06425
Jason Smale <jsmale@zendesk.com>
2012-09-22 17:09:53 +1000

    Nice clean example of users rendering

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,36 +6,10 @@ App = Em.Application.create({
   storeNamespace: 'Tracker',
   ApplicationController: Em.Controller.extend(),
   UsersController: Em.ArrayController.extend(),
+  ActivitiesController: Em.ArrayController.extend(),
   ready: function() {
     this.initialize();
   }
-});
-
-/* Routes */
-App.Router = Em.Router.extend({
-  root: Em.Route.extend({
-
-    index: Em.Route.extend({
-      route: '/',
-      enter: function(router) {
-        console.log("entering root.index from", router.getPath('currentState.name'));
-      },
-      connectOutlets: function(router) {
-        router.get('applicationController').connectOutlet('users', App.User.find());
-      }
-    }),
-
-    activities: Em.Route.extend({
-      route: '/activities',
-      enter: function(router) {
-
-      },
-      connectOutlets: function(router) {
-        router.get('applicationController').connectOutlet('activites');
-      }
-    })
-
-  })
 });
 
 /* Models */
@@ -57,51 +31,6 @@ App.User.reopenClass({
   }
 });
 
-
-// var user = App.User.create({
-//   id: '1'
-// });
-
-// console.log(user);
-// console.log(user.fetch().done(function(d){
-//   console.log(d);
-// }));
-// console.log();
-
-/*App.Exercise = Em.Object.extend({
-  id: null,
-  title: null
-});
-
-App.Activity = Em.Object.extend({
-  id: null,
-  description: null,
-  exercise_id: null,
-  user_id: null
-});*/
-
-
-/*App.User.create({
-  id: '1',
-  name: 'Jason',
-  email: 'jason.smale@gmail.com',
-  phone: '0402073344',
-  displayed: true
-}),
-App.User.create({
-  id: '2',
-  name: 'Sian',
-  email: 'sian.smale@gmail.com',
-  phone: '0402073344',
-  displayed: true
-})*/
-
-/* Controllers */
-
-App.ActivitiesController = Em.ArrayController.extend({
-  content: []
-});
-
 App.ApplicationView = Em.ContainerView.extend({
   elementId: 'awesome-app',
   childViews: ['outletView'],
@@ -111,16 +40,29 @@ App.ApplicationView = Em.ContainerView.extend({
 });
 
 /* Views */
-App.UsersView = Em.CollectionView.extend({
-  contentBinding: "controller.content",
-  classNames: ['users-list'],
-  itemViewClass: Em.View.extend({
-    tagName: 'a',
-    template: Em.Handlebars.compile('{{view.content.name}}')
-  })
+App.UsersView = Em.View.extend({
+  templateName: 'users'
 });
 
-App.ActivitesView = Em.CollectionView.extend({
-  contentBinding: 'controller.content',
-  elementId: 'activites'
+/* Router */
+App.Router = Em.Router.extend({
+  root: Em.Route.extend({
+
+    showActivitesInput: Ember.Route.transitionTo('activities'),
+
+    index: Em.Route.extend({
+      route: '/',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('users', App.User.find());
+      }
+    }),
+
+    activities: Em.Route.extend({
+      route: '/activites/:user',
+      connectOutlets: function(router, context) {
+        router.get('applicationController').connectOutlet('activites', context);
+      }
+    })
+
+  })
 });
